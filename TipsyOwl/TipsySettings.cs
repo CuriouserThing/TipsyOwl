988630331086fc7dcfb57c334ef3fcc5432b3738
TheMostCuriousThing <adelaidekwalter@gmail.com>
2020-11-17 13:01:56 -0500

    Add field for latest game version to settings struct.

--- TipsyOwl/TipsySettings.cs
+++ TipsyOwl/TipsySettings.cs
@@ -4,6 +4,8 @@ namespace TipsyOwl
 {
     public class TipsySettings
     {
+        public IList<int> LatestVersion { get; set; } = new List<int>(capacity: 0);
+
         public IList<string> HiddenCardKeywords { get; set; } = new List<string>(capacity: 0);
 
         public IDictionary<string, ulong> RegionIconEmotes { get; set; } = new Dictionary<string, ulong>(capacity: 0);
