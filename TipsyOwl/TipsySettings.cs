8027aba776c008f09bacbce731b38b8509deef01
TheMostCuriousThing <adelaidekwalter@gmail.com>
2020-11-17 20:40:06 -0500

    To init-only.

--- TipsyOwl/TipsySettings.cs
+++ TipsyOwl/TipsySettings.cs
@@ -4,18 +4,18 @@ namespace TipsyOwl
 {
     public class TipsySettings
     {
-        public IList<int> LatestVersion { get; set; } = new List<int>(capacity: 0);
+        public IList<int> LatestVersion { get; init; } = new List<int>(capacity: 0);
 
-        public IList<string> HiddenCardKeywords { get; set; } = new List<string>(capacity: 0);
+        public IList<string> HiddenCardKeywords { get; init; } = new List<string>(capacity: 0);
 
-        public IDictionary<string, ulong> RegionIconEmotes { get; set; } = new Dictionary<string, ulong>(capacity: 0);
+        public IDictionary<string, ulong> RegionIconEmotes { get; init; } = new Dictionary<string, ulong>(capacity: 0);
 
-        public IDictionary<string, ulong> RegionIndicatorEmotes { get; set; } = new Dictionary<string, ulong>(capacity: 0);
+        public IDictionary<string, ulong> RegionIndicatorEmotes { get; init; } = new Dictionary<string, ulong>(capacity: 0);
 
-        public IDictionary<string, ulong> SetIconEmotes { get; set; } = new Dictionary<string, ulong>(capacity: 0);
+        public IDictionary<string, ulong> SetIconEmotes { get; init; } = new Dictionary<string, ulong>(capacity: 0);
 
-        public IDictionary<string, IList<string>> KeywordSprites { get; set; } = new Dictionary<string, IList<string>>(capacity: 0);
+        public IDictionary<string, IList<string>> KeywordSprites { get; init; } = new Dictionary<string, IList<string>>(capacity: 0);
 
-        public IDictionary<string, ulong> SpriteEmotes { get; set; } = new Dictionary<string, ulong>(capacity: 0);
+        public IDictionary<string, ulong> SpriteEmotes { get; init; } = new Dictionary<string, ulong>(capacity: 0);
     }
 }
