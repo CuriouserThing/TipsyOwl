d467bd9752a6112c7581dbb355ec1fa6f4e3115f
TheMostCuriousThing <adelaidekwalter@gmail.com>
2020-12-26 02:58:41 -0500

    Get rid of suppressions file.

--- TipsyOwl/GlobalSuppressions.cs
+++
@@ -1,2 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0045:Convert to conditional expression")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:Convert to conditional expression")]
